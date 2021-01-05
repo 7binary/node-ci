1ccf69d17688f40948e34147e6a59429fdc512d1
Zinoviev Artem <7binary@gmail.com>
2021-01-05 22:42:12 +0300

    Updated travis-ci config

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use('/auth', createProxyMiddleware({ target: 'http://localhost:5000' }));
-  app.use('/api', createProxyMiddleware({ target: 'http://localhost:5000' }));
+  const PORT = process.env.PORT || 5000;
+  console.log('> PORT middleware', PORT);
+
+  app.use('/auth', createProxyMiddleware({ target: `http://localhost:${PORT}` }));
+  app.use('/api', createProxyMiddleware({ target: `http://localhost:${PORT}` }));
 };
